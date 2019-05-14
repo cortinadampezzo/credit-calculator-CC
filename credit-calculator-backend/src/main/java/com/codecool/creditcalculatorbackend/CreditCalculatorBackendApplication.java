8f2a12530c4911cda72bc6bdadd4a096d0f2cb6c
cortinadampezzo <kisgans@gmail.com>
2019-05-14 15:27:08 +0200

    initial commit

---
+++ credit-calculator-backend/src/main/java/com/codecool/creditcalculatorbackend/CreditCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.creditcalculatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCalculatorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCalculatorBackendApplication.class, args);
+    }
+
+}
