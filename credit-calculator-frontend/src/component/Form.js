cf0b1658525b93ee1d79e788418b2bf052886ae4
cortinadampezzo <kisgans@gmail.com>
2019-06-12 14:00:57 +0200

    bugfix

--- credit-calculator-frontend/src/component/Form.js
+++ credit-calculator-frontend/src/component/Form.js
@@ -11,7 +11,7 @@ class Form extends Component {
 
         this.state = {
             data: {
-            loanAmount: null
+            loanAmount: null,
             interestRate: null,
             repaymentTime: null,
             monthlyPayment: null}
